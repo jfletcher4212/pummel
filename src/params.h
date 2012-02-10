14505f73fe24b8b2eddc22b8b1708a21c3f101d4
fletcher4212 <fletcher4212@localhost>
2012-02-09 18:35:33 -0800

    Added the updated Gantt Chart (as of 2/2/12) to /doc/gantt

---
+++ src/params.h
@@ -0,0 +1,30 @@
+/* params.h - provides the paramaters for object.
+   Made by Coleman Beaseley - 12/16/2011
+  */
+#ifndef PARAMS_H
+#define PARAMS_H
+
+using namespace std;
+
+//parameters for shapes
+struct shapeParams{
+    string name;
+    QString fillColor;
+    int weight;
+};
+
+//parameters for lines
+struct lineParams{
+    QString color;
+    int weight;
+};
+
+//parameters for text
+struct textParams{
+    QString font;
+    QString color;
+    int size;
+};
+
+
+#endif // PARAMS_H
