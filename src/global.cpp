0fd8f7ff26cd7406e1e862fb01a911aaa4f28a2d
Alex Dean <ad31337@gmail.com>
2012-02-15 13:53:49 -0800

    Merged alex changes

---
+++ src/global.cpp
@@ -0,0 +1,6 @@
+#include "drawarea.h"
+// Testing Mercurial commit
+// Testing 123
+
+DrawArea *canvas;
+
---
+++ src/markerbox.cpp
@@ -0,0 +1,20 @@
+#include "markerbox.h"
+
+MarkerBox::MarkerBox(QGraphicsItem *parent) : QGraphicsItem(parent){
+    width = 5;
+    height = 5;
+    setOpacity(1.0);
+}
+
+QRectF MarkerBox::boundingRect() const{
+    return QRectF(0,0,width,height);
+}
+
+void MarkerBox::paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget){
+    if(painter == 0){
+        // make a painter if none exists
+        painter = new QPainter();
+    }
+    painter->setBrush(Qt::black);
+    painter->drawRect(QRectF(0, 0,width,height));
+}
