70f826926daba8283d9969465688ee12573bdc80
davyyd <davyyd@gmail.com>
2012-03-05 12:34:21 -0800

    Merged with integration.

---
+++ src/drawarea.cpp
@@ -0,0 +1,52 @@
+#include <QBoxLayout>
+#include "drawarea.h"
+#include "dragscene.h"
+#include "dragview.h"
+#include "dragitem.h"
+
+DrawArea::DrawArea(QWidget *parent, int newHeight, int newWidth){
+    scene = new DragScene(0, newHeight, newWidth);
+    view = new DragView();
+    layout = new QHBoxLayout();
+    view->setAcceptDrops(true);
+    view->setScene(scene);
+    layout->addWidget(view);
+    this->setLayout(layout);
+}
+
+void DrawArea::setSceneCreateMode(ShapeType newType){
+    scene->setCreateMode(newType);
+}
+
+void DrawArea::setArrowSceneCreateMode(LineType newType)
+{
+   scene->setArrowCreateMode(newType);
+}
+
+ShapeType DrawArea::getSceneCreateMode(){
+    return scene->getCreateMode();
+}
+
+void DrawArea::setSceneCreate(bool a){
+    scene->setSceneCreate(a);
+}
+
+bool DrawArea::getSceneCreate(){
+    return scene->getSceneCreate();
+}
+
+bool DrawArea::getSceneGrid(){
+    return scene->getGrid();
+}
+
+int DrawArea::getSceneGridSize(){
+    return scene->getGridSize();
+}
+
+void DrawArea::setSceneGrid(bool a){
+    scene->setGrid(a);
+}
+
+void DrawArea::setSceneGridSize(int newSize){
+    scene->setGridSize(newSize);
+}
---
+++ src/ellipse.h
@@ -0,0 +1,45 @@
+#ifndef ELLIPSE_H
+#define ELLIPSE_H
+
+#include "icon.h"
+#include <QtGui>
+#include <QGraphicsItem>
+
+#include "markerbox.h"
+class ellipse : public icon
+{
+
+
+protected:
+    QRectF boundingRect() const;
+    void paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget = 0);
+    void mouseMoveEvent(QGraphicsSceneMouseEvent *event);
+    void mousePressEvent(QGraphicsSceneMouseEvent *event);
+    void mouseReleaseEvent(QGraphicsSceneMouseEvent *event);
+
+private:
+    // DragItem id counter         // also done in parent class
+    //static int next_id;
+
+    // DragItem parameters
+    //int width;
+    //int height;
+    //ShapeType shape;
+    //int id;
+
+    // Selection boxes for the DragItem
+    MarkerBox *markers[4];
+
+public:
+
+    ellipse(QGraphicsItem *parent = 0);
+    ellipse(QGraphicsItem *parent, int xsize, int ysize, int xpos, int ypos);
+
+    // Accessors            // done in parent class
+    //int getWidth();
+    //int getHeight();
+    //int getId();
+
+};
+
+#endif // ELLIPSE_H
