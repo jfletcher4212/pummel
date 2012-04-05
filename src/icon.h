36cbc4a705f6be3574528fd65626c915e75b26ed
tengutinker <tengutinker@localhost>
2012-04-05 16:53:23 -0700

    added QPolygonF

--- src/icon.h
+++ src/icon.h
@@ -7,53 +7,40 @@
 #include <QWidget>
 #include <QGraphicsItem>
 #include <QPointF>
-#include "markerbox.h"
 
-class Icon : public QGraphicsItem
+//int next_ID = 1;
+
+class icon : public QGraphicsItem
 {
 protected:
 
-    QRectF boundingRect() const{return QRectF(0,0,m_width, m_height);}
-    void mousePressEvent(QGraphicsSceneMouseEvent *event);
-    void mouseMoveEvent(QGraphicsSceneMouseEvent *event);
-    void mouseReleaseEvent(QGraphicsSceneMouseEvent *event);
-
-    int m_width;
-    int m_height;
-    static int m_next_id;
-    int m_id;
-
-    QString m_shapetype;
-//    QString m_label;
-    QGraphicsTextItem *m_labelBox;
-    QPolygon *m_type;
+    int m_iD;                       //ID for the item
+    QPointF m_pos;                  //Position identifier for canvas
+    int m_xsize;                    //width
+    int m_ysize;                    //height
+    QString m_shapetype;            //name of shape (A.K.A. "ellipse", "classbox", etc.)
+//    QString m_label;                //the text in the box
+    QGraphicsTextItem *m_labelbox;  //textbox
+    QPolygon m_bound;               //bounding rectangle of the item.
     QImage m_image;          //holds the imported image to be drawn
-    int m_state; // used for zValue stacking and item selection
-    MarkerBox *m_markers[4]; // resizing boxes
 
+    static int m_next_id;
 
 public:
 
-    Icon(QGraphicsItem *parent = 0);
-
-    // Accessors
-    int getWidth();
-    int getHeight();
-    int getState();
-    MarkerBox* getMarkerBox(int x);
+    icon(QGraphicsItem *parent = 0);
+    int getXSize();
+    int getYSize();
+    void setSize(int newXSize, int newYSize);  // simple accessor and mutator methods for the size
     QString reportShapetype();
     void setShapetype(QString shapename);     // simple accessor and mutator methods for the shapetype
-    int getID();    // simple accessor for getting the ID
-    QPolygon *getType();
-
-    // Mutators
-    void setMarkers(MarkerBox* a, MarkerBox* b, MarkerBox* c, MarkerBox* d);
-    void setState(int x);
-    void setSize(int newXSize, int newYSize);  // simple accessor and mutator methods for the size
+    int getID();                              // simple accessor for getting the ID
+    void mousePressEvent(QGraphicsSceneMouseEvent *event);
+    void mouseMoveEvent(QGraphicsSceneMouseEvent *event);
+    void mouseReleaseEvent(QGraphicsSceneMouseEvent *event);
+    QPolygon getData();
+    void setPolyType();
     void setText(QString input);		//change the value of m_labelbox
-
-    // Utility
-    void paintMarkerBoxes();
 };
 
 #endif // shapes_H
