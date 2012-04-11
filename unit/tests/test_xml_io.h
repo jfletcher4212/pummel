46cbe8e1c40bc9bd09ad226c35b5b9897503fff7
cabljohn <cabljohn@localhost>
2012-04-10 19:43:54 -0700

    properly writing xml documents

--- unit/tests/test_xml_io.h
+++ unit/tests/test_xml_io.h
@@ -2,42 +2,47 @@
          
 #include <cxxtest/TestSuite.h>
 #include "../src/xml_io.h"
-#include "dummy_icon.h"
 #include "../../src/actor.h"
+#include "../../src/ellipse.h"
+#include "../../src/classbox.h"
 
 class ut_xml_io : public CxxTest::TestSuite {
 public:
   void test_write_xml(void)
   {
-    int size = 2;
+    int size = 5;
     Xml_io *test;
 
     QList<Icon*> list;
     QString filename = "Testfile";
     QString diagram_type = "testtype";
     
-    Icon *temp;
-    temp = new actor();
-    //temp = new Dummy_icon_test();
-    delete temp;
-    
-    /*
     for( int i = 0; i < size; i++ )
-      {
-        Icon *temp = new ClassBox();
+    {
+        Icon *temp;
+	
+	if ( i % 2 == 0 )
+	{
+	    temp = new actor();
+	}
+	else
+	{
+	    temp = new Ellipse();
+	}
+	
 	list.append(temp);
-      }
-    */
-    //test = new Xml_io(list, filename);
+    }
+    
+    test = new Xml_io(list, filename);
     //test = new Xml_io();
 
-    //test->write_xml();
-    /*
+    test->write_xml();
+    
     for (int i = 0; i < size; i++ )
-      {
+    {
 	delete list[i];
-      }
-    */
+    }
+    
     delete test;
 
   }  
