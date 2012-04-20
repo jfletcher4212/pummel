c9f66588377adebfc0e9f70593bc29732020da44
ckbeasley <ckbeasley@localhost>
2012-04-19 21:39:18 -0700

    merged changes from integration

--- src/senariostate.cpp
+++ src/senariostate.cpp
@@ -1,18 +1,19 @@
-#include "ellipse.h"
+#include "senariostate.h"
 #include "markerbox.h"
 #include <iostream>
 #include <QString>
 
 
-Ellipse::Ellipse(QGraphicsItem *parent) : Icon(parent)
+SenarioState::SenarioState(QGraphicsItem *parent) : Icon(parent)
 {
     // allows setting of the base rectangle of dragitem
-    m_shapetype = "Ellipse";
+    m_shapetype = "SenarioState";
 
     m_labelBox->setParentItem(this);
     m_labelBox->setFlag(QGraphicsItem::ItemIsSelectable, false);
 
-    m_labelBox->setPlainText("Ellipse");
+    m_label= "SenarioState";
+    m_labelBox->setPlainText(m_label);
 
     m_labelBox->setPos(this->pos());
     arrangeBoxes();
@@ -20,21 +21,21 @@ Ellipse::Ellipse(QGraphicsItem *parent) : Icon(parent)
     m_labelBox->setVisible(true);
     // In general, leave this block below running for loading images
 
-    if (!m_image.load("icons/ellipse.png"))
+    if (!m_image.load("icons/senarioState.png"))
         std::cout << "didn't load image properly\n";  //loads the image for drawing later
 
 
     // If the relative path fails, uncomment and update the below with full path to images (example below from a mac)
-    //if (!m_image.load("/Users/mac/Desktop/Rev78-build-desktop-Desktop_Qt_4_7_4_for_GCC__Qt_SDK__Debug/icons/ellipse.png"))
-        std::cout << "didn't load image properly\n";  //loads the image for drawing later
+    //if (!m_image.load("/Users/mac/Desktop/Rev78-build-desktop-Desktop_Qt_4_7_4_for_GCC__Qt_SDK__Debug/icons/senarioState.png"))
+      //  std::cout << "didn't load image properly\n";  //loads the image for drawing later
 }
 
-Ellipse::Ellipse(QGraphicsItem *parent, int xsize, int ysize, int xpos, int ypos, QString label) : Icon(parent)
+SenarioState::SenarioState(QGraphicsItem *parent, int xsize, int ysize, int xpos, int ypos, QString label) : Icon(parent)
 {
     // allows setting of the base rectangle of dragitem
     m_width = xsize;
     m_height = ysize;
-    m_shapetype = "Ellipse";
+    m_shapetype = "SenarioState";
 
     this->setPos(xpos,ypos);
 
@@ -42,7 +43,7 @@ Ellipse::Ellipse(QGraphicsItem *parent, int xsize, int ysize, int xpos, int ypos
     m_labelBox->setFlag(QGraphicsItem::ItemIsSelectable, false);
 
     m_label = label;
-    m_labelBox->setPlainText(label);
+    m_labelBox->setPlainText(m_label);
 
     m_labelBox->setPos(this->pos());
     arrangeBoxes();
@@ -51,21 +52,21 @@ Ellipse::Ellipse(QGraphicsItem *parent, int xsize, int ysize, int xpos, int ypos
 
     // In general, leave this block below running for loading images
 
-   // if (!m_image.load("icons/ellipse.png"))
-     //   std::cout << "didn't load image properly\n";  //loads the image for drawing later
+    if (!m_image.load("icons/senarioState.png"))
+        std::cout << "didn't load image properly\n";  //loads the image for drawing later
 
     // If the relative path fails, uncomment and update the below with full path to images (example below from a mac)
-    if (!m_image.load("/Users/mac/Desktop/Rev78-build-desktop-Desktop_Qt_4_7_4_for_GCC__Qt_SDK__Debug/icons/ellipse.png"))
-        std::cout << "didn't load image properly\n";  //loads the image for drawing later
+    //if (!m_image.load("/Users/mac/Desktop/Rev78-build-desktop-Desktop_Qt_4_7_4_for_GCC__Qt_SDK__Debug/icons/senarioState.png"))
+    //    std::cout << "didn't load image properly\n";  //loads the image for drawing later
 
 }
 
-QRectF Ellipse::boundingRect() const
+QRectF SenarioState::boundingRect() const
 {
     return QRectF(0,0,m_width, m_height);
 }
 
-void Ellipse::paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget)
+void SenarioState::paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget)
 {
     m_labelBox->boundingRect().setHeight(m_height-20);
     m_labelBox->boundingRect().setWidth(m_width-20);
@@ -80,17 +81,18 @@ void Ellipse::paint(QPainter *painter, const QStyleOptionGraphicsItem *option, Q
     painter->setPen(Qt::NoPen);
 
     painter->drawImage(this->boundingRect(),m_image);
+
     update();
 
 }
 
-void Ellipse::mouseDoubleClickEvent(QGraphicsSceneMouseEvent *event)
+void SenarioState::mouseDoubleClickEvent(QGraphicsSceneMouseEvent *event)
 {
     event->accept();
     setValues();
 }
 
-void Ellipse::setValues()
+void SenarioState::setValues()
 {
     //open dialog box for user editing
     TextBoxDialog *values = new TextBoxDialog(this);
@@ -99,7 +101,7 @@ void Ellipse::setValues()
     arrangeBoxes();
 }
 
-void Ellipse::arrangeBoxes()
+void SenarioState::arrangeBoxes()
 {
     m_labelBox->setPos(((m_width*0.5)-(m_labelBox->boundingRect().width()*0.5)), ((m_height*0.5)-(m_labelBox->boundingRect().height()*0.5)));
    // (int)m_labelBox->boundingRect().width();
@@ -116,6 +118,14 @@ void Ellipse::arrangeBoxes()
     if(m_labelBox->boundingRect().height()+20 > m_height)
     {
           m_height = m_labelBox->boundingRect().height() +20;
+    }
+    if(m_height>m_width)
+    {
+        m_width=m_height;
+    }
+    else
+    {
+        m_height=m_width;
     }
     paintMarkerBoxes();
     update();
