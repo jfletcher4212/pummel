27e9028f7608aa162da2581790481526b7ac62f7
cabljohn <cabljohn@localhost>
2012-04-20 14:16:16 -0700

    starting fresh

---
+++ src/classbox.h
@@ -0,0 +1,39 @@
+/* classbox.h
+ * ClassBox
+ * 3 QGraphicsTextItems for the class name, class members and class methods
+ * TO-DO:   Modify double-click behavior so that the object doesn't move immediately when clicked
+ */
+//
+#ifndef CLASSBOX_H
+#define CLASSBOX_H
+
+#include <QWidget>
+#include <QGraphicsItem>
+#include <QtGui>
+#include <iostream>
+#include "icon.h"
+
+class ClassBox : public Icon
+{
+private:
+    QGraphicsTextItem *m_memberBox, *m_methodBox;
+    int m_titleBorder, m_memberBorder, m_methodBorder;
+    QString m_members, m_methods;
+    void setRect();
+    void arrangeBoxes();
+
+protected:
+    QRectF boundingRect() const;
+    void paint(QPainter *painter, const QStyleOptionGraphicsItem *, QWidget * = 0);
+    void mouseDoubleClickEvent(QGraphicsSceneMouseEvent *);
+    void setValues();
+
+public:
+    ClassBox();
+    ClassBox(QGraphicsItem *parent, int xsize, int ysize, int xpos, int ypos, QString label, QString members, QString methods);
+    ~ClassBox();
+    QString getLabel(), getMembers(), getMethods();
+    void setLabel(QString), setMembers(QString), setMethods(QString);
+};
+
+#endif // CLASSBOX_H
