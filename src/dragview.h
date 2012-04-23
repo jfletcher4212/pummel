89bf0f0294964bb61dfc2628b2c98bc0f72b235b
norris2033 <norris2033@gmail.com>
2012-04-23 03:17:49 -0700

    merged with integration

---
+++ src/dragview.h
@@ -0,0 +1,20 @@
+/*
+ * Reimplementation of QGraphicsView
+ * Displays DragScene
+ *
+ */
+
+#ifndef DRAGVIEW_H
+#define DRAGVIEW_H
+
+#include <QtGui>
+#include <QGraphicsView>
+
+class DragView : public QGraphicsView
+{
+public:
+    DragView(QWidget *parent = 0);
+
+};
+
+#endif // DRAGVIEW_H
