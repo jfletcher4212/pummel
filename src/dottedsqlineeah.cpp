1bca43414ef4a2eb4f666d384809d3f31545d838
norris2033 <norris2033@gmail.com>
2012-04-24 14:24:32 -0700

    Merged in More Lines

---
+++ src/dottedsqlineeah.cpp
@@ -0,0 +1,87 @@
+#include "dottedsqlineeah.h"
+
+const qreal Pi = 3.14;
+
+dottedsqlineeah::dottedsqlineeah(Icon *sourceReferenceObj, Icon *destinationReferenceObj, QGraphicsItem *parent = 0, QGraphicsScene *scene = 0)
+    :lineBody(sourceReferenceObj, destinationReferenceObj, parent, scene)
+{
+    parent = 0;
+    scene = 0;
+    myLineType = Solid_Square_Line;
+}
+
+dottedsqlineeah::~dottedsqlineeah()
+{
+
+}
+
+void dottedsqlineeah::paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget)
+{
+    option = 0;
+    widget = 0;
+
+    if (mySourceReferenceObj->collidesWithItem(myDestinationReferenceObj))
+        return;
+
+    qreal arrowSize = 20;
+
+    painter->setBrush(Qt::white);
+    painter->setPen(QPen(myColor, 2, Qt::DashLine, Qt::RoundCap, Qt::RoundJoin));
+
+    QPointF point1 = mySourceReferenceObj->pos();
+    QPointF point4 = myDestinationReferenceObj->pos();
+
+    point1.rx() += 0.5 * mySourceReferenceObj->getWidth();
+    point1.ry() += 0.5 * mySourceReferenceObj->getHeight();
+
+    point4.rx() += 0.5 * myDestinationReferenceObj->getWidth();
+    point4.ry() += 0.5 * myDestinationReferenceObj->getHeight();
+
+   QPointF point2 = point1;
+   QPointF point3;
+
+    QLineF tempAngleLine(point1, point4);
+    this->setLine(tempAngleLine);
+
+    qreal angle = line().angle();
+
+   if((angle > 45 && angle < 135) || (angle > 225 && angle < 315))
+   {
+       point2.ry() += (point4.ry() - point1.ry())/2;
+       point3 = point2;
+       point3.rx() += point4.rx() - point1.rx();
+   }
+
+   else
+   {
+       point2.rx() += (point4.rx() - point1.rx())/2;
+       point3 = point2;
+       point3.ry() += point4.ry() - point1.ry();
+   }
+
+
+    QLineF tempLineOne(point1, point2);
+    QLineF tempLineTwo(point2, point3);
+    QLineF tempLineThree(point3, point4);
+
+    QPointF interPoint = findIntersection(myDestinationReferenceObj, tempLineThree);
+
+    this->setLine(QLineF(interPoint, point3));
+
+    double ahAngle = this->getAngle(interPoint, mySourceReferenceObj);
+
+    QPointF arrowP1 = line().p1() + QPointF(sin(ahAngle + Pi / 3)*arrowSize, cos(ahAngle + Pi / 3) *arrowSize);
+    QPointF arrowP2 = line().p1() + QPointF(sin(ahAngle + Pi - Pi / 3) * arrowSize, cos(ahAngle + Pi - Pi / 3) * arrowSize);
+    arrowHead.clear();
+    arrowHead << line().p1() << arrowP1 << arrowP2;
+
+    painter->drawLine(tempLineOne);
+    painter->drawLine(tempLineTwo);
+    painter->drawLine(line());
+
+    painter->setPen(QPen(myColor, 2, Qt::SolidLine, Qt::RoundCap, Qt::RoundJoin));
+
+    painter->drawPolygon(arrowHead);
+
+    update();
+}
---
+++ src/solidsqlinesah.h
@@ -0,0 +1,16 @@
+#ifndef SOLIDSQLINESAH_H
+#define SOLIDSQLINESAH_H
+#include "linebody.h"
+
+class solidsqlinesah : public lineBody
+{
+public:
+    solidsqlinesah(Icon *, Icon *, QGraphicsItem *, QGraphicsScene *);
+    ~solidsqlinesah();
+
+protected:
+    void paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget = 0);
+
+};
+
+#endif // SOLIDSQLINESAH_H
