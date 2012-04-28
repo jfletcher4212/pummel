0b0d6d3d146648395d703187e1cbdfa65aa1b5de
cabljohn <cabljohn@localhost>
2012-04-28 15:26:01 -0700

    more xml ut

--- unit/tests/test_xml_io.h
+++ unit/tests/test_xml_io.h
@@ -9,7 +9,6 @@
 #include "../../src/note.h"
 #include "../../src/roundedsquare.h"
 #include "../../src/scenarioend.h"
-#include "../../src/scenariostate.h"
 #include "../../src/scenariostart.h"
 #include "../../src/scenariostart.h"
 
@@ -25,28 +24,44 @@ public:
     QList<Icon*> list;
     QString filename = "Testfile.ut";
     QString diagram_type = "testtype";
+    Icon *temp;    
+
+    // actor
+    tmp = new Actor(0, 23, 23, 23, 23, (QString)"test_actor");
+    list.append(tmp);
+      
+    // ellipse
+    tmp = new Ellipse(0, 23, 23, 23, 23, (QString)"test_ellipse");
+    list.append(tmp);
     
-    for( int i = 0; i < size; i++ )
-    {
-        Icon *temp;
-	
-	//if ( i % 2 == 0 )
-	//{
-	    //temp = new Actor();
-	//}
-	//else
-	//{
-	temp = new Ellipse();
-	    //}
-	
-	list.append(temp);
-	//delete temp;
-    }
+    // classbox
+    tmp = new ClassBox(0, 23, 23, 23, 23, (QString)"test_classbox::+::and::+::this");
+    list.append(tmp);
+        
+    // note
+    tmp = new Note(0, 23, 23, 23, 23, (QString)"test_note");
+    list.append(tmp);
+        
+    // roundedsquare
+    tmp = new RoundedSquare(0, 23, 23, 23, 23, (QString)"test::+::roundedsquare")
+    list.append(tmp);
+        
+    // scenariostart
+    tmp = new ScenarioStart(0, 23, 23, 23, 23);
+    list.append(tmp);
+        
+    // scenarioend
+    tmp = new ScenarioEnd(0, 23, 23, 23, 23);      
+    list.append(tmp);
+    
     test = new Xml_io(filename);
     test->set_items(list);
-    //test = new Xml_io();
-
+    
+    // run the test
     test->write_xml();
+    
+    // write an expected file and diff?
+    // or verify things can be read out?
     
     for (int i = 0; i < size; i++ )
     {
@@ -69,32 +84,62 @@ public:
       delete test;
   }
   
+  // do instantiation better per class
   void test_parse_icon(void)
   {
-      Xml_io *test = new Xml_io();
+      QString filename = "tmpfile.ut";
+      Xml_io *test = new Xml_io(filename);
       Icon *current;
       
-      // ellipse
-      current = new Ellipse();      
-      TS_ASSERT( help_test_parse_icon(test, current) );
-      //help_test_parse_icon(test, current);
+      // actor
+      current = new Actor(0, 23, 23, 23, 23, (QString)"test_actor");
+      help_test_parse_icon(test, current);
       delete current;
+
+      // ellipse
+      current = new Ellipse(0, 23, 23, 23, 23, (QString)"test_ellipse");
+      help_test_parse_icon(test, current);
+      delete current;
+
+      // classbox
+      current = new ClassBox(0, 23, 23, 23, 23, (QString)"test_classbox::+::and::+::this");
+      help_test_parse_icon(test, current);
+      delete current;
+
+      // note
+      current = new Note(0, 23, 23, 23, 23, (QString)"test_note");
+      help_test_parse_icon(test, current);
+      delete current;
+
+      // roundedsquare
+      current = new RoundedSquare(0, 23, 23, 23, 23, (QString)"test::+::roundedsquare");
+      help_test_parse_icon(test, current);
+      delete current;
+
+      // scenariostart
+      current = new ScenarioStart(0, 23, 23, 23, 23);
+      help_test_parse_icon(test, current);
+      delete current;
+
+      // scenarioend
+      current = new ScenarioEnd(0, 23, 23, 23, 23);      
+      help_test_parse_icon(test, current);
+      delete current;
+
       
       delete test;
   }
   
-  int help_test_parse_icon(Xml_io *test, Icon *input)
+  void help_test_parse_icon(Xml_io *test, Icon *input)
   {
       int result;
       Icon *output;
 
-      /// set icon properties
-      
+      // setup
       QList<Icon*> list;
       list.append(input);
       QString filename = "tmpfile.ut";
-      
-      // setup
+
       test->set_items(list);
       test->set_filename(filename);
       test->write_xml();
@@ -105,41 +150,43 @@ public:
       
       // run the test
       output = test->parse_icon(reader);
-
-      /*
-      result = ( 
-	  (TS_ASSERT_EQUALS( input->getWidth(), output->getWidth() ))
-	  && (TS_ASSERT_EQUALS( input->getHeight(), output->getHeight() ))
-	  && (TS_ASSERT_EQUALS( input->get_xPos(), output->get_xPos() ))
-	  && (TS_ASSERT_EQUALS( input->get_yPos(), output->get_yPos() ))
-	  && (TS_ASSERT_EQUALS( input->reportShapetype(), output->reportShapetype() ))
-	  && (TS_ASSERT_EQUALS( input->get_all(), output->get_all() ))
-	  );
-      */
-      /*
-      TS_ASSERT_EQUALS( input->getWidth(), output->getWidth() );
-      TS_ASSERT_EQUALS( input->getHeight(), output->getHeight() );
-      TS_ASSERT_EQUALS( input->get_xPos(), output->get_xPos() );
-      TS_ASSERT_EQUALS( input->get_yPos(), output->get_yPos() );
-      TS_ASSERT_EQUALS( input->reportShapetype(), output->reportShapetype() );
-      TS_ASSERT_EQUALS( input->get_all(), output->get_all() );
-      */
-      result = (
-	  (input->getWidth() == output->getWidth())
-	  && (input->getHeight() == output->getHeight())
-	  && (input->get_xPos() == output->get_xPos())
-	  && (input->get_yPos() == output->get_yPos())
-	  && (input->reportShapetype() == output->reportShapetype())
-	  && (input->get_all() == output->get_all())
-	  );
       
+      TS_ASSERT(input->getWidth() == output->getWidth());
+      TS_ASSERT(input->getHeight() == output->getHeight());
+      TS_ASSERT(input->get_xPos() == output->get_xPos());
+      TS_ASSERT(input->get_yPos() == output->get_yPos());
+      TS_ASSERT(input->reportShapetype() == output->reportShapetype());
+      TS_ASSERT(input->get_all() == output->get_all());
+
+      // cleanup
       delete output;
-      return result;
+  }
+  
+  void test_make_icon(void)
+  {
+      QString filename = "tmpfile.ut";
+      Xml_io *test = new Xml_io(filename);
+      Icon *current;
+      
+      QString val0 = "Actor";
+      QString val1 = "test_actor";
+      int val2 = 23;
+      int val3 = 23;
+      int val4 = 23;
+      int val5 = 23;
+      
+      // actor
+      current = test->make_icon(val0, val2, val3, val4, val5, val1);
+      TS_ASSERT(current);
+      
+      delete current;
+      delete test;
   }
   
   void test_choose_type(void)
   {
-      Xml_io *test = new Xml_io();
+      QString filename = "tmpfile.ut";
+      Xml_io *test = new Xml_io(filename);
       
       TS_ASSERT_EQUALS(test->choose_type((DiagramType)Class), (QString)"Class");
       TS_ASSERT_EQUALS(test->choose_type((DiagramType)StateChart), (QString)"StateChart");
