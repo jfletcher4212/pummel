b8307cfc446e270695b95557e8107e2fd8338ee5
davyyd <davyyd@localhost>
2012-04-28 15:09:43 -0700

    added test_lineBody.h for unit testing.

---
+++ unit/tests/test_lineBody.h
@@ -0,0 +1,14 @@
+#include <cxxtest/TestSuite.h>
+#include "../src/lineBody.h"
+
+using namespace std;
+
+
+class ut_lineBody : public CxxTest::TestSuite {
+public:
+  void test_getState(void)
+  {
+        cout<<"Testing\n";
+        }
+};
+
