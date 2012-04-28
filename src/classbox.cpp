b8be9d5213eef6afeb55893e7001af98cfde4bc0
ckbeasley <ckbeasley@localhost>
2012-04-28 01:19:14 -0700

    added helper methods to ClassBox.\nUsed for testing.

--- src/classbox.cpp
+++ src/classbox.cpp
@@ -87,7 +87,7 @@ void ClassBox::arrangeBoxes()
 
     //change m_height and m_width
     //match the width of the overall boundary rectangles to the widest one
-    /*
+    
     if(m_width < m_memberBox->boundingRect().width() || m_width < m_methodBox->boundingRect().width()
             || m_width < m_labelBox->boundingRect().width())
     {
@@ -106,7 +106,7 @@ void ClassBox::arrangeBoxes()
             m_width = m_labelBox->boundingRect().width();
         }
     }
-    */
+    /*
     if(m_width < m_labelBox->boundingRect().width())
     {
         m_width = m_labelBox->boundingRect().width();
@@ -119,7 +119,7 @@ void ClassBox::arrangeBoxes()
     {
         m_width = m_methodBox->boundingRect().width();
     }
-
+    */
 
     if((m_labelBox->boundingRect().height()
             + m_memberBox->boundingRect().height()
@@ -181,11 +181,11 @@ void ClassBox::paint(QPainter *painter, const QStyleOptionGraphicsItem *option, 
     painter->drawRect(m_labelBox->pos().x(), (int)m_labelBox->pos().y(), m_width, m_labelBox->boundingRect().height());
     painter->drawRect(m_memberBox->pos().x(), (int)m_memberBox->pos().y(), m_width, m_memberBox->boundingRect().height());
     painter->drawRect(m_methodBox->pos().x(), (int)m_methodBox->pos().y(), m_width, m_methodBox->boundingRect().height());
-    /*
+/*
     painter->drawRect(m_memberBox->pos().x(), m_memberBox->pos().y(), m_width, memberBoundary.height());
     painter->drawRect(m_methodBox->pos().x(), m_methodBox->pos().y(), m_width, methodBoundary.height());
     painter->drawRect(m_labelBox->pos().x(), m_labelBox->pos().y(), m_width, labelBoundary.height());
-
+*/
 /*
     QRectF mine = this->boundingRect();
     pen.setColor(Qt::red);
@@ -205,14 +205,7 @@ void ClassBox::mouseDoubleClickEvent(QGraphicsSceneMouseEvent *event)
 
 QRectF ClassBox::boundingRect() const
 {
-
-    return QRectF(0,0, m_methodBox->boundingRect().width(),
-                  m_labelBox->boundingRect().height() + m_memberBox->boundingRect().height()
-                  + m_methodBox->boundingRect().height());
-
-/*
     return QRectF(0, 0, m_width, m_height);
-*/
 }
 
 //Called when the ClassBox is double-clicked
@@ -275,3 +268,12 @@ QStringList ClassBox::split_all(QString value)
     return value.split(delim);
 }
 
+int ClassBox::setWidthsManually(int labelWidth, int memberWidth, int methodWidth)
+{
+	m_labelBox->boundingRect().setWidth(labelWidth);
+	m_memberBox->boundingRect().setWidth(memberWidth);
+	m_methodBox->boundingRect().setWidth(methodWidth);
+	arrangeBoxes();
+
+	return m_width;
+}
