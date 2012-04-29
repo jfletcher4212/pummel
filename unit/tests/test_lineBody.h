0f053fb56a31783ed4a8d860a7466a376d5f2729
davyyd <davyyd@localhost>
2012-04-28 17:39:24 -0700

    Added code to test_lineBody.h

--- unit/tests/test_lineBody.h
+++ unit/tests/test_lineBody.h
@@ -1,5 +1,7 @@
 #include <cxxtest/TestSuite.h>
-#include "../src/lineBody.h"
+#include <iostream>
+#include "../../src/solidline.h"
+#include "../../src/classbox.h"
 
 using namespace std;
 
@@ -8,7 +10,26 @@ class ut_lineBody : public CxxTest::TestSuite {
 public:
   void test_getState(void)
   {
-        cout<<"Testing\n";
+        cout<<"\nTesting\n";
         }
+
+  void test_findObjectCenter(void)
+	{
+
+		//QString tmpString = "Testfile.ut";
+		//Icon *MyTestClassbox = new ClassBox (0, 5, 5, 0, 0, tmpString);
+		//Icon *testCB = NULL; 
+		lineBody *testLB = NULL;
+
+		testLB = new solidline(NULL, NULL, 0, 0);
+
+		//lineBody->findObjectCenter(testCB);
+	
+		//QPointF testCenter = MyTestLine->findObjectCenter (MyTestClassbox);	
+
+		//cout<<"\ntestCenter is: " << testCenter << "\n";
+
+	}
+
 };
 
