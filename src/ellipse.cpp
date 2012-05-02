ca03cddf722ad2473110d4dbe3de4f70c7c90bab
hobojoe00 <hobojoe00@localhost>
2012-05-01 18:53:11 -0700

    rounded square is now in mvc

--- src/ellipse.cpp
+++ src/ellipse.cpp
@@ -8,27 +8,17 @@ Ellipse::Ellipse(QGraphicsItem *parent) : Icon(parent)
 {
     // allows setting of the base rectangle of dragitem
     m_shapetype = "Ellipse";
-
-    //m_labelBox->setParentItem(this);
-    //m_labelBox->setFlag(QGraphicsItem::ItemIsSelectable, false);
-
     m_height /= 1.5;
-    
     m_label = "Use Case";
-
-    //m_labelBox->setPlainText(m_label);
 
     m_labelheight = m_labelwidth = 20;
 
-    //m_labelBox->setPos(this->pos());
     arrangeBoxes();
 
-    //m_labelBox->setVisible(true);
     // In general, leave this block below running for loading images
 
     if (!m_image.load("icons/ellipse.png"))
         std::cout << "didn't load image properly\n";  //loads the image for drawing later
-
 
     // If the relative path fails, uncomment and update the below with full path to images (example below from a mac)
     //if (!m_image.load("/Users/mac/Desktop/Rev78-build-desktop-Desktop_Qt_4_7_4_for_GCC__Qt_SDK__Debug/icons/ellipse.png"))
@@ -47,17 +37,10 @@ Ellipse::Ellipse(QGraphicsItem *parent, int id, int xsize, int ysize, int xpos, 
     m_yPos = ypos;
     this->setPos(m_xPos,m_yPos);
 
-    //m_labelBox->setParentItem(this);
-    //m_labelBox->setFlag(QGraphicsItem::ItemIsSelectable, false);
-
     m_label = label;
-    //m_labelBox->setPlainText(label);
     m_labelheight = m_labelwidth = 20;
 
-    //m_labelBox->setPos(this->pos());
     arrangeBoxes();
-
-    //m_labelBox->setVisible(true);
 
     // In general, leave this block below running for loading images
 
@@ -88,8 +71,6 @@ void Ellipse::paint(QPainter *painter, const QStyleOptionGraphicsItem *option, Q
 
     m_labelBox->boundingRect().setHeight(m_height-20);
     m_labelBox->boundingRect().setWidth(m_width-20);
-    m_labelBox->setPlainText(m_label);
-
 
     m_labelBox->setPlainText(m_label);
 
@@ -132,11 +113,6 @@ void Ellipse::setValues()
 
 void Ellipse::arrangeBoxes()
 {
-   // m_labelBox->setPos(((m_width*0.5)-(m_labelBox->boundingRect().width()*0.5)), ((m_height*0.5)-(m_labelBox->boundingRect().height()*0.5)));
-   // (int)m_labelBox->boundingRect().width();
-   // (int)m_labelBox->boundingRect().height();
-
-
     this->prepareGeometryChange();
 
     //change m_height and m_width
