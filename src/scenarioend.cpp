14ef6b037f40267643746a819f333c6c994103d6
fletcher4212 <fletcher4212@localhost>
2012-05-01 19:54:32 -0700

    Fixed menus, added keyboard shortcuts and made exit work

---
+++ src/scenarioend.cpp
@@ -0,0 +1,74 @@
+#include "scenarioend.h"
+#include "markerbox.h"
+#include <iostream>
+#include <QString>
+
+
+ScenarioEnd::ScenarioEnd(QGraphicsItem *parent) : Icon(parent)
+{
+    // allows setting of the base rectangle of dragitem
+    m_shapetype = "ScenarioEnd";
+
+    // In general, leave this block below running for loading images
+    m_width= 25;
+    m_height= 25;
+
+    if (!m_image.load("icons/scenarioEnd.png"))
+        std::cout << "didn't load image properly\n";  //loads the image for drawing later
+
+
+    // If the relative path fails, uncomment and update the below with full path to images (example below from a mac)
+    //if (!m_image.load("/Users/mac/Desktop/Rev78-build-desktop-Desktop_Qt_4_7_4_for_GCC__Qt_SDK__Debug/icons/ScenarioEnd.png"))
+      //  std::cout << "didn't load image properly\n";  //loads the image for drawing later
+}
+
+ScenarioEnd::ScenarioEnd(QGraphicsItem *parent, int id, int xsize, int ysize, int xpos, int ypos) : Icon(parent)
+{
+    // allows setting of the base rectangle of dragitem
+    m_id = id;
+    m_width = xsize;
+    m_height = ysize;
+    m_shapetype = "ScenarioEnd";
+
+    this->setPos(xpos,ypos);
+
+    // In general, leave this block below running for loading images
+
+    if (!m_image.load("icons/ScenarioEnd.png"))
+        std::cout << "didn't load image properly\n";  //loads the image for drawing later
+
+    // If the relative path fails, uncomment and update the below with full path to images (example below from a mac)
+    //if (!m_image.load("/Users/mac/Desktop/Rev78-build-desktop-Desktop_Qt_4_7_4_for_GCC__Qt_SDK__Debug/icons/ScenarioEnd.png"))
+    //    std::cout << "didn't load image properly\n";  //loads the image for drawing later
+
+}
+
+QRectF ScenarioEnd::boundingRect() const
+{
+    return QRectF(0,0,m_width, m_height);
+}
+
+void ScenarioEnd::paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget)
+{
+
+    if(m_width>m_height)
+    {
+        m_height=m_width;
+    }
+    else
+    {
+        m_width=m_height;
+    }
+    if(painter == 0)
+    {
+        // make a painter if none exists
+        painter = new QPainter();
+    }
+    painter->setPen(Qt::NoPen);
+
+    painter->drawImage(this->boundingRect(),m_image);
+
+    update();
+
+}
+
---
+++ src/selfrefline.h
@@ -0,0 +1,17 @@
+#ifndef SELFREFLINE_H
+#define SELFREFLINE_H
+
+#include "linebody.h"
+
+class selfRefLine : public lineBody
+{
+public:
+    selfRefLine(Icon *, Icon *, QGraphicsItem *, QGraphicsScene *);
+    selfRefLine(Icon *, Icon *, int,  int, QGraphicsItem *, QGraphicsScene *);
+    selfRefLine(int id_start, int id_end);
+
+protected:
+    void paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget = 0);
+};
+
+#endif // SELFREFLINE_H
