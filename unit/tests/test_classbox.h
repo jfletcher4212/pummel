6500be40bcfbe349a0015f6deab990b534c06f95
ckbeasley <ckbeasley@localhost>
2012-05-07 19:41:55 -0700

    worked on testing

--- unit/tests/test_classbox.h
+++ unit/tests/test_classbox.h
@@ -15,18 +15,10 @@ public:
 		ClassBox box(QPointF(0,0));
 
 		cout << endl << "***CLASSBOX INSTANTIATION***" << endl;
-		//check on initial size
-		cout << "\nBOUNDING RECT INITIAL MEASUREMENTS\nHeight: " << box.boundingRect().height() << ". Width: " << box.boundingRect().width() << endl;
-		cout << "m_height: " << box.getHeight() << ". m_width: " << box.getWidth() << endl;
-
 		//check initial contents
 		TS_ASSERT(box.getLabel().compare("Name") == 0);
 		TS_ASSERT(box.getMembers().compare("Members") == 0);
 		TS_ASSERT(box.getMethods().compare("Methods") == 0);
-		cout << "\nINITIAL VALUES\nLabel:\t\t'" << box.getLabel().toStdString() 
-			<< "'\nMembers:\t'" << box.getMembers().toStdString() 
-			<< "'\nMethods:\t'" << box.getMethods().toStdString() << "'" << endl;
-
 	}
 
 	void test_load_instance(void)
@@ -46,7 +38,6 @@ public:
 
 		TS_ASSERT_EQUALS(box.m_id, id);
 		TS_ASSERT_EQUALS(box.m_width, width);
-//		box.boundingRect().setHeight(height);
 		TS_ASSERT_EQUALS(box.m_height, height);
 		TS_ASSERT_EQUALS(box.m_xPos, xpos);
 		TS_ASSERT_EQUALS(box.m_yPos, ypos);
@@ -62,34 +53,11 @@ public:
 		TS_ASSERT(quick.compare("Methods") == 0);
 
 		box.arrangeBoxes();
-//		box.paint(0,0,0);
-//		box.m_labelBox->setPlainText("giggity");
-//		cout << box.m_labelBox->toPlainText() << endl;
 		//check dimensions
 		int new_height;
 		new_height = box.m_labelBox->boundingRect().height()
 			+ box.m_memberBox->boundingRect().height()
 			+ box.m_methodBox->boundingRect().height() + 60;
 		TS_ASSERT_EQUALS(box.m_height, new_height);
-	}
-	//this test does not work. No feasible way to resize individual boxes.
-	void test_arrangeBoxes(void)
-	{
-/*
-		cout << endl << "***CLASSBOX AUTO-RESIZING***" << endl;
-
-		ClassBox box(QPointF(0,0));
-		int boxWidth[3];
-
-		boxWidth[0] = box.setWidthsManually(300, 150, 299);
-		boxWidth[1] = box.setWidthsManually(299, 350, 299);
-		boxWidth[2] = box.setWidthsManually(299, 299, 300);
-
-		//print boxWidths
-			cout << "Label width = 300. m_width = " << box.setWidthsManually(300, 150, 299) << endl;
-			
-			cout << "Memberbox width = 350. m_width = " << boxWidth[1] << endl;
-			cout << "Methodbox width = 300. m_width = " << boxWidth[2] << endl;
-*/
 	}
 };
