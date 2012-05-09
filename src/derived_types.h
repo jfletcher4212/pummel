e7dadff600c1702e8209c5e5719e80dfe15944f5
davyyd <davyyd@localhost>
2012-05-08 17:30:13 -0700

    Merged davyyd from integration.

---
+++ src/derived_types.h
@@ -0,0 +1,35 @@
+#ifndef __DERIVED_TYPES_H__
+#define __DERIVED_TYPES_H__
+
+// icons
+#include "actor.h"
+#include "classbox.h"
+#include "ellipse.h"
+#include "note.h"
+#include "roundedsquare.h"
+#include "scenarioend.h"
+#include "scenariostart.h"
+#include "scenariostart.h"
+
+// lines
+#include "solidline.h"
+#include "dottedline.h"
+#include "solidlinesah.h"
+#include "dottedlinesah.h"
+#include "solidlineeah.h"
+#include "dottedlineeah.h"
+#include "solidlinesd.h"
+#include "dottedlinesd.h"
+#include "solidlineed.h"
+#include "dottedlineed.h"
+#include "solidlinebah.h"
+#include "dottedlinebah.h"
+#include "solidsqline.h"
+#include "dottedsqline.h"
+#include "solidsqlinesah.h"
+#include "dottedsqlinesah.h"
+#include "solidsqlineeah.h"
+#include "dottedsqlineeah.h"
+#include "selfrefline.h"
+
+#endif // __DERIVED_TYPES_H__
