fc01da71e27d56be184ffbc0128f95dd4ad85123
cabljohn <cabljohn@localhost>
2012-05-09 13:25:25 -0700

    more classbox ut

---
+++ src/solidsqlinebah.h
@@ -0,0 +1,16 @@
+#ifndef SOLIDSQLINEBAH_H
+#define SOLIDSQLINEBAH_H
+#include "linebody.h"
+
+class solidsqlinebah : public lineBody
+{
+public:
+    solidsqlinebah(Icon *, Icon *, QGraphicsItem *, QGraphicsScene *);
+    solidsqlinebah(int id_start, int id_end);
+    solidsqlinebah(Icon *, Icon *, int,  int, QGraphicsItem *, QGraphicsScene *);
+
+protected:
+    void paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget = 0);
+};
+
+#endif // SOLIDSQLINEBAH_H
