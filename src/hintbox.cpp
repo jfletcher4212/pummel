dc36ed9d5b3fd977bc19a3fd0063893e9b4a3a69
cabljohn <cabljohn@localhost>
2012-05-09 17:32:55 -0700

    updating hint

--- src/hintbox.cpp
+++ src/hintbox.cpp
@@ -36,9 +36,9 @@ void HintBox::initMembers()
     hints.append("1. To exit line insertion mode, click on an empty portion of the canvas.\n");
     hints.append("2. To exit icon insertion mode, click on an existing icon.\n");
     hints.append("3. To edit an icon's text, double click the icon\n");
-    hints.append("4. Exit using File->Exit\n");
-    hints.append("5. Future hints go here!");
-
+    hints.append("4. To create a note, click the 'Note' button\n");
+    hints.append("5. Exit using File->Exit\n");
+    
     m_textBox->setPlainText(hints);
 }
 
